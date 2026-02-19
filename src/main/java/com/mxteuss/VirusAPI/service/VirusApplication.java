b5546d6bc321ce72cd9e3baa1eacc265b0932b9d
mxteuss <ovagabundant@gmail.com>
2026-02-19 18:47:02 -0300

    feat: Instanciando service

---
+++ src/main/java/com/mxteuss/VirusAPI/service/VirusApplication.java
@@ -0,0 +1,23 @@
+package com.mxteuss.VirusAPI.service;
+
+import com.mxteuss.VirusAPI.service.service.UrlService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class VirusApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(VirusApplication.class, args);
+
+        UrlService urlService = context.getBean(UrlService.class);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira a URL: ");
+        String url = scanner.nextLine();
+
+        urlService.scanURL(url);
+    }
+}
